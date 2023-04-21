6fccb52d78539a8dc2ac8bfb4a53f76b5647753e
cachenow <36331626+cachenow@users.noreply.github.com>
2023-04-21 16:05:57 +0800

    Update ylive.sh

--- ylive.sh
+++ ylive.sh
@@ -15,7 +15,7 @@ read -p "是否启用 screen 会话? (y/n) " use_screen
 if [[ "$use_screen" == "y" ]]; then
   # 启用 screen 会话
   read -p "请输入 screen 会话名称: " session_name
-  screen -dmS $session_name bash -c "streamlink --http-header 'User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3' -O $youtube_url best | ffmpeg -re -i pipe:0 -c:v libx264 -preset veryfast -tune zerolatency -b:v 5000k -maxrate 5500k -bufsize 6000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv -strict -2 $rtmp_url/$stream_name"
+  screen -dmS $session_name bash -c "streamlink --http-header 'User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3' -O $youtube_url best | ffmpeg -re -i pipe:0 -c:v libx264 -preset veryfast -tune zerolatency -b:v 2000k -maxrate 3000k -bufsize 3000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv -strict -2 $rtmp_url/$stream_name"
 else
   # 不启用 screen 会话
   streamlink --http-header "User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" -O $youtube_url best | ffmpeg -re -i pipe:0 -c:v libx264 -preset veryfast -tune zerolatency -b:v 2000k -maxrate 3000k -bufsize 3000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 128k -ac 2 -ar 44100 -f flv -strict -2 $rtmp_url/$stream_name
